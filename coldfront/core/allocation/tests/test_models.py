e327255ffbcdd3f13e3f92d24aee7a263401779a
Eric Butcher <107886303+Eric-Butcher@users.noreply.github.com>
2025-07-23 15:29:58 -0400

    Added basic tests ensuring the correct behavior is used depending on the arguments used. Some tests are not passing do not bugs in other methods.

--- coldfront/core/allocation/tests/test_models.py
+++ coldfront/core/allocation/tests/test_models.py
@@ -12,16 +12,21 @@ from django.utils import timezone
 
 from coldfront.core.allocation.models import (
     Allocation,
+    AllocationAttribute,
     AllocationStatusChoice,
 )
 from coldfront.core.project.models import Project
 from coldfront.core.test_helpers.factories import (
+    AllocationAttributeFactory,
+    AllocationAttributeTypeFactory,
     AllocationFactory,
     AllocationStatusChoiceFactory,
     ProjectFactory,
     ResourceFactory,
 )
-
+import string
+import random
+import pickle
 
 class AllocationModelTests(TestCase):
     """tests for Allocation model"""
@@ -140,3 +145,146 @@ class AllocationModelCleanMethodTests(TestCase):
             status=self.active_status, start_date=start_and_end_date, end_date=start_and_end_date, project=self.project
         )
         actual_allocation.full_clean()
+
+class AllocationModelGetAttributeTests(TestCase):
+    
+    # | attr | expand | typed | situation code |
+    # |    0 |      0 |     0 | nothing        |
+    # |    0 |      0 |     1 | nothing        |
+    # |    0 |      1 |     0 | nothing        |
+    # |    0 |      1 |     1 | nothing        |
+    # |    1 |      0 |     0 | value          |
+    # |    1 |      0 |     1 | typed_value    |
+    # |    1 |      1 |     0 | expanded_value |
+    # |    1 |      1 |     1 | expanded_value |
+
+    @staticmethod
+    def __random_string(domain: str):
+        # TODO: apply the FAKE_SEED to this random number generator once that infrastructure is put in
+        return ''.join(random.choices(domain, k=random.randint(1, 8)))
+    
+    # TODO: replace magic_number with STRESS_LEVEL once that infrastructure is put in
+    magic_number = 10
+
+    def setUp(self):
+        """
+        Create an Allocation and associate AllocationAttributes with AllocationAttributeTypes 
+        with type names following a sequence of integers.
+        """
+        self.allocation = AllocationFactory()
+        for n in range(self.magic_number):
+            undesired_type_name = str(n)
+            undesired_allocation_attribute_type = AllocationAttributeTypeFactory(name=undesired_type_name)
+            undesired_allocation_attribute = AllocationAttributeFactory(allocation=self.allocation, allocation_attribute_type=undesired_allocation_attribute_type)
+
+
+    def _setup_and_run_for_no_attr_tests(self, expand: bool, typed: bool):
+        """
+        Pass the boolean arguments expand and typed into Allocation.get_attribute
+        and assert that nothing changed and None was returned. The name is generated 
+        randomly but will be guaranteed to never be the name of any other AllocationAttributeTypes
+        placed in the database by the function. 
+        """
+        
+        before_allocation_state = pickle.dumps(self.allocation.refresh_from_db())
+
+        desired_type_name = self.__random_string(string.ascii_letters)
+        actual = self.allocation.get_attribute(name=desired_type_name, expand=expand, typed=typed)
+
+        after_allocation_state = pickle.dumps(self.allocation.refresh_from_db())
+
+        self.assertIsNone(actual)
+        self.assertEqual(before_allocation_state, after_allocation_state)
+
+    def _setup_for_attr_tests(self) -> tuple[AllocationAttribute, str]:
+        """
+        Create a random name and an AllocationAttribute with an associated 
+        AllocationAttributeFactory which are associated with the self.allocation 
+        and return that AllocationAttribute and the name generated as a tuple.
+        """
+        desired_type_name = self.__random_string(string.ascii_letters)
+        desired_allocation_attribute_type = AllocationAttributeTypeFactory(name=desired_type_name)
+        desired_allocation_attribute = AllocationAttributeFactory(allocation=self.allocation, allocation_attribute_type=desired_allocation_attribute_type)
+        desired_allocation_attribute.refresh_from_db()
+        return desired_allocation_attribute, desired_type_name
+
+
+    def test_no_attr_exists_and_not_expand_and_not_typed_does_nothing(self):
+        expand = False
+        typed = False
+        self._setup_and_run_for_no_attr_tests(expand, typed)
+
+
+    def test_no_attr_exists_and_not_expand_and_typed_does_nothing(self):
+        expand = False
+        typed = True
+        self._setup_and_run_for_no_attr_tests(expand, typed)
+
+    def test_no_attr_exists_and_expand_and_not_typed_does_nothing(self):
+        expand = True
+        typed = False
+        self._setup_and_run_for_no_attr_tests(expand, typed)
+
+    def test_no_attr_exists_and_expand_and_typed_does_nothing(self):
+        expand = True
+        typed = True
+        self._setup_and_run_for_no_attr_tests(expand, typed)
+
+    def test_attr_exists_and_not_expand_and_not_typed_returns_simple_value(self):
+        desired_allocation_attribute, desired_name = self._setup_for_attr_tests()
+
+        actual = self.allocation.get_attribute(desired_name, expand=False, typed=False)
+        expected = desired_allocation_attribute.value
+
+        self.assertIsInstance(actual, str)
+        self.assertEqual(actual, expected)
+
+
+    def test_attr_exists_and_not_expand_and_typed_returns_typed_value(self):
+        desired_allocation_attribute, desired_name = self._setup_for_attr_tests()
+
+        actual = self.allocation.get_attribute(desired_name, expand=False, typed=True)
+        expected = desired_allocation_attribute.typed_value()
+
+        self.assertIsInstance(actual, str)
+        self.assertEqual(actual, expected)
+
+    def test_attr_exists_and_expand_and_not_typed_returns_expanded_value(self):
+        desired_allocation_attribute, desired_name = self._setup_for_attr_tests()
+
+        actual = self.allocation.get_attribute(desired_name, expand=True, typed=False)
+        expected = desired_allocation_attribute.expanded_value()
+
+        self.assertIsInstance(actual, str)
+        self.assertEqual(actual, expected)
+
+    def test_attr_exists_and_expand_and_typed_returns_expanded_value(self):
+        desired_allocation_attribute, desired_name = self._setup_for_attr_tests()
+
+        actual = self.allocation.get_attribute(desired_name, expand=True, typed=True)
+        expected = desired_allocation_attribute.expanded_value()
+
+        self.assertIsInstance(actual, str)
+        self.assertEqual(actual, expected)
+
+    def test_no_attr_exists_default_kwargs_does_nothing(self):
+        before_allocation_state = pickle.dumps(self.allocation.refresh_from_db())
+
+        desired_type_name = self.__random_string(string.ascii_letters)
+        actual = self.allocation.get_attribute(desired_type_name)
+
+        after_allocation_state = pickle.dumps(self.allocation.refresh_from_db())
+
+        self.assertIsNone(actual)
+        self.assertEqual(before_allocation_state, after_allocation_state)
+
+    def test_attr_exists_default_kwargs_returns_expanded_value(self):
+        desired_allocation_attribute, desired_name = self._setup_for_attr_tests()
+
+        actual = self.allocation.get_attribute(desired_name)
+        expected = desired_allocation_attribute.expanded_value()
+
+        self.assertIsInstance(actual, str)
+        self.assertEqual(actual, expected)
+
+
